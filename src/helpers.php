f2861a8791a12a970fa0861a5976d95dbf17fd8f
Arvid de Jong <arvid@darvis.nl>
2023-12-15 10:45:50 +0100

    Helpers updated

--- src/helpers.php
+++ src/helpers.php
@@ -2,89 +2,78 @@
 
 use MarvinLabs\Html\Bootstrap\Bootstrap;
 
-if ( !function_exists('bs'))
-{
-
-    /**
-     * @return \MarvinLabs\Html\Bootstrap\Bootstrap
-     */
-    function bs()
+/**
+ * Retrieve the Bootstrap instance.
+ *
+ * @return \MarvinLabs\Html\Bootstrap\Bootstrap
+ */
+if (!function_exists('bs')) {
+    function bs(): Bootstrap
     {
         return app(Bootstrap::class);
     }
 }
 
-if ( !function_exists('data_attributes'))
-{
-
-    /**
-     * @param array $attrs The data attributes as an associative array (key will be appended after 'data-', value will
-     *                     be used as attribute value)
-     *
-     * @return string The data attributes usable directly within the HTML tag
-     */
-    function data_attributes(array $attrs = [])
+/**
+ * Generate a string of HTML data attributes from an associative array.
+ *
+ * @param array $attrs Associative array of data attributes.
+ * @return string The data attributes usable directly within the HTML tag.
+ */
+if (!function_exists('data_attributes')) {
+    function data_attributes(array $attrs = []): string
     {
         return collect($attrs)
             ->map(function ($value, $key) {
-                return 'data-' . $key . '="' . $value . '"';
+                return 'data-' . $key . '="' . htmlspecialchars($value, ENT_QUOTES) . '"';
             })
             ->implode(' ');
     }
 }
 
-if ( !function_exists('field_name_to_id'))
-{
-
-    /**
-     * Converts a valid form field name to a valid HTML ID. Transforms all dots and square brackets to some sane
-     * separators accepted with ID strings.
-     *
-     * @param string $name
-     * @param string $suffix
-     *
-     * @return string
-     */
-    function field_name_to_id($name, $suffix = '')
+/**
+ * Converts a form field name to a valid HTML ID, with optional suffix.
+ *
+ * @param string $name Form field name to convert.
+ * @param string $suffix Optional suffix to append.
+ * @return string Converted HTML ID.
+ */
+if (!function_exists('field_name_to_id')) {
+    function field_name_to_id(string $name, string $suffix = ''): string
     {
-        if (empty($name))
-        {
+        if ($name === '') {
             return $name;
         }
 
         $out = str_replace(['.', '[]', '[', ']'], ['_', '', '_', ''], $name);
-        return empty($suffix) ? $out : "${out}_${suffix}";
+        return isset($suffix) && $suffix !== '' ? "{$out}_{$suffix}" : $out;
     }
 }
 
-if ( !function_exists('in_array_any'))
-{
-    /**
-     * Check if any of the needles exist within the haystack
-     *
-     * @param array $needles
-     * @param array $haystack
-     *
-     * @return bool
-     */
-    function in_array_any($needles, $haystack) {
+/**
+ * Check if any of the specified values exist in the array.
+ *
+ * @param array $needles Values to search for.
+ * @param array $haystack Array to search in.
+ * @return bool True if any value is found, false otherwise.
+ */
+if (!function_exists('in_array_any')) {
+    function in_array_any(array $needles, array $haystack): bool
+    {
         return (bool) array_intersect($needles, $haystack);
     }
 }
 
-if ( !function_exists('in_array_all'))
-{
-
-    /**
-     * Check if all needles exist within the haystack
-     *
-     * @param array $needles
-     * @param array $haystack
-     *
-     * @return bool
-     */
-    function in_array_all($needles, $haystack) {
-        return !array_diff($needles, $haystack);
+/**
+ * Check if all of the specified values exist in the array.
+ *
+ * @param array $needles Values to search for.
+ * @param array $haystack Array to search in.
+ * @return bool True if all values are found, false otherwise.
+ */
+if (!function_exists('in_array_all')) {
+    function in_array_all(array $needles, array $haystack): bool
+    {
+        return !array_diff_assoc($needles, $haystack);
     }
 }
-
